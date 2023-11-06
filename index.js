371ff0971b5a7eafadf6f78150a3c09d418dae54
tuhinhossain123 <csetuhin55@gmail.com>
2023-11-06 18:43:42 +0600

    user data sever side add

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors =require ('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -26,6 +26,26 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const serviceCollection =client.db('offlineService').collection('services');
+
+
+    app.get('/services', async(req, res)=>{
+      const cursor = serviceCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+
+    })
+
+    app.post('/services', async(req, res)=>{
+      console.log(req)
+      const user = req.body;
+      const result =await serviceCollection.insertOne(user);
+      res.send(result)
+    })
+
+    
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
